aa1be42135c6b66f274b637cf2aa25b73967cacc
cindy kivala <cindykivala@gmail.com>
2025-06-21 20:17:45 +0300

    send data with fetch

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Add your code here
+function submitData(name, email) {
+    const formData = {
+        name: name,
+        email: email,
+    };
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+        body: JSON.stringify(formData),
+    })
+    .then((response) => response.json())
+    .then((object) => {
+        const idElement = document.createElement("p");
+        idElement.textContent = `ID: ${object.id}`;
+        document.body.appendChild(idElement);
+    })
+    .catch((error) => {
+        const errorElement = document.createElement("p");
+        errorElement.textContent = `Error: ${error.message}`;
+        document.body.appendChild(errorElement);
+    });
+}
